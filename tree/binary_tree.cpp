e42575552ac47483561861ed167d97f61caecfb1
YCHEN-NYU <yc1224@nyu.edu>
2017-01-23 23:11:08 -0500

    save for further implementation

--- tree/binary_tree.cpp
+++ tree/binary_tree.cpp
@@ -1,4 +1,20 @@
 #include "binary_tree.h"
 using namespace std;
 
+// Constructor for the BTree, set up the root for the BTree
+BTree::BTree() {
+  root = new node;
+  root->left = nullptr;
+  root->right = nullptr;
+  count = 0;
+  current_node = root;
+}
 
+// destructor: TODO
+BTree::~BTree() {
+  node * temp_node = root;
+
+}
+
+// insert a node by the quantity
+BTree::insert_node(int quantity) { }
---
+++ tree/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+  BTree Car_Flow = BTree();
+  Car_Flow.print_tree();
+
+  return 0;
+}
